f9daea8b0e49e7a65948ec10e7f9c580713c708b
testuser <jiangyue0380@gmail.com>
2023-05-31 12:23:35 +1000

    created SiteHeader.js as the header for the app

---
+++ src/pages/SiteHeader/SiteHeader.js
@@ -0,0 +1,19 @@
+const SiteHeader = () => {
+    return(
+        <div>     
+            <p>logo</p>  
+            <nav>
+            <ul>
+                <li>home</li>
+                <li>collection</li>
+                <li>wish list</li>
+            </ul>
+            </nav>
+            <button>sign in</button>
+            <button>sign out</button>
+        </div>
+
+    )
+}
+
+export default SiteHeader
