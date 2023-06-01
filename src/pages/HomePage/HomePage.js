9e4240f6de9deabca94858a8c58370aa1dec2233
testuser <jiangyue0380@gmail.com>
2023-06-01 14:54:30 +1000

    updated HomePage.js with useAccount hook from the msal-react package to retrive logged in user information

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
-import { useMsal } from '@azure/msal-react';
 import { loginRequest } from '../../context/authConfig';
 
+//https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/hooks.md
+import { useMsal, useAccount } from '@azure/msal-react';
+
 const HomePage = () => {
-    const {instance, accounts} = useMsal();
-    const [user, setUser] = useState(null);
-    console.log(instance)
-    console.log(accounts)
+    // const {instance, accounts} = useMsal();
+    const user = useAccount(accounts[0] || {});
 
-    if(accounts.length > 0 && !user) 
-    {
-        instance.acquireTokenSilent({
-            ...loginRequest,
-            account: accounts[0],
-        }).then(response => {
-            setUser(response.account);
-        });
-    }
-
+    // if(accounts.length > 0 && !user) 
+    // {
+    //     instance.acquireTokenSilent({
+    //         ...loginRequest,
+    //         account: accounts[0],
+    //     }).then(response => {
+    //         setUser(response.account);
+    //     });
+    // }
+    console.log(user)
     return (        
     <div>
         <p>Hello {user.name}</p>
